9f139dc4bfecc4f28335e19b3f130cd86c23640e
CristianUser <cristianmejia97@gmail.com>
2020-04-26 18:07:19 -0400

    Create DB schemas (#1)
    * auth working
    
    * files moved
    
    * comand updated
    
    * using es5
    
    * add missing entities
    
    * added some docs
    
    * update agradon
    
    * user schema updated

--- app/index.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-import express from 'express';
-
-const app = express();
-
-app.listen(process.env.PORT, () => console.log(`Server is listening on port ${process.env.PORT}`));
-
-export default app;
